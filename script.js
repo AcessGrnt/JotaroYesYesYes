bef4a5eff60dc20abd5d21141957d1e1dd17f2b3
João Pedro Fagundes <70774421+AcessDnd@users.noreply.github.com>
2022-03-24 23:17:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const yesYes = document.getElementById('Trap');
+const noNo = document.getElementById('Mulher');
+const esperando = document.getElementById('espe');
+const norm = document.getElementById('Normal');
+
+function yesyes () {
+    esperando.src = './img/yes.gif';
+}
+
+function nono () {
+    esperando.src = './img/no.gif';
+}
+
+function none () {
+    esperando.src = './img/esperando.gif'
+}
+
+yesYes.addEventListener('click', yesyes);
+noNo.addEventListener('click', nono);
+norm.addEventListener('click', none);
